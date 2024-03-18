6b37dd11caf6c80199c5518814824e0b3030a201
chegecolins <chegecolins@gmail.com>
2024-03-18 18:50:49 +0000

    ed

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["plugin:@next/next/recommended", 
-    "next",],
-  rules: {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
-  },
-};
